1dcc035f3790ccd6549086507a62dbda5429cc0f
tarde <gyalvarado@hotmail.com>
2023-11-30 18:50:25 +0100

    Initial commit

---
+++ src/com/corenetworks/presentacion/ProbarDado.java
@@ -0,0 +1,14 @@
+package com.corenetworks.presentacion;
+
+import com.corenetworks.modelo.dado;
+
+public class PruebaDado {
+    public static void main(String[] args) {
+
+dado d1= new dado();
+d1.setdado();
+        System.out.println(d1.getdado());
+
+
+    }
+}
